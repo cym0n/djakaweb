6330be0a71930bbd5e07242495f8a9dd2786557d
cymon <cymon@chrome.it>
2011-10-08 15:46:42 +0200

    Template Toolkit plugged in

--- bin/app.pl
+++ bin/app.pl
@@ -1,8 +1,11 @@
 #!/usr/bin/env perl
 use Dancer;
 use Dancer::Plugin::DBIC;
+use Dancer::Template::TemplateToolkit;
 use DjakaWeb;
 use DjakaWeb::Elements::Game;
+
+ set layout => 'djaka';
 
 get '/home' => sub {
 	my $user = schema->resultset('Game')->find('1');
@@ -24,6 +27,13 @@ get '/login/:id' => sub {
 		$game = DjakaWeb::Elements::Game->new({'id' => $game_id});
 		return redirect '/game/' . $game->id();
 	}
+};
+
+get '/game/:game' => sub {
+	template 'interface';
+
+
+
 };
 
 
