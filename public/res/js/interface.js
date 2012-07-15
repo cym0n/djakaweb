5c2e926e92f4ed225a7168b2877bcc3b382c0a49
cym0n <cymon@tiscali.it>
2012-07-15 22:11:29 +0200

    Intelligence frontend Label for danger bar Little page refactoring

--- public/res/js/interface.js
+++ public/res/js/interface.js
@@ -1,20 +1,20 @@
+function showLog()
+{
+	$('#inttip').removeClass('active');
+	$('#logtip').addClass('active');
+	$('#intelligence').hide();
+	$('#messages').show();
+}
+function showIntelligence()
+{
+	$('#intelligence').html("Clicca su INFO di un elemento per averne la descrizione");
+	$('#inttip').addClass('active');
+	$('#logtip').removeClass('active');
+	$('#intelligence').show();
+	$('#messages').hide();
+}
+
 $(document).ready(function() {
-		//$('.actions_trigger[rel]').overlay({
-		//		top: "center",
-		//		onBeforeLoad: function() {
-		//			var id = this.getTrigger().attr('id');
-		//			var callfun = "/game/service/actions/"+id;
-		//			var menu = this.getOverlay();
-		//			$.ajax({ url: callfun,
-		//	 				 async: true,
-		//   				 complete: function(XMLHttpRequest, textStatus)
-		//	 				 {
-		//		 				var output=XMLHttpRequest.responseText;
-		//		 				menu.html(output);
-		//					 }
-		//					});
-		//			 }	
-		//	});
 		$('.actions_trigger').click(function() {
 				var id = $(this).parent().attr('id');
 				var callfun = "/game/service/actions/"+id;
@@ -28,7 +28,21 @@ $(document).ready(function() {
 				});
 				return false;
 			});
-
-
-
+		$('.info_trigger').click(function() {
+				var id = $(this).parent().attr('id');
+				var callfun = "/game/service/description/"+id;
+				$.ajax({ url: callfun,
+		 				 async: true,
+		   				 complete: function(XMLHttpRequest, textStatus)
+		 				 {
+			 				var output=XMLHttpRequest.responseText;
+			 				$('#intelligence').html(output);
+							$('#logtip').removeClass('active');
+							$('#inttip').addClass('active');
+							$('#messages').hide();
+							$('#intelligence').show();
+						 }
+				});
+				return false;
+			});
 });
